8941411117bad0d25513f923ae923c847929044a
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-09-12 12:04:44 +0100

    creating configure the docker compose file

---
+++ book-network/src/main/java/com/bsn/book_network/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.bsn.book_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
---
+++ book-network/src/test/java/com/bsn/book_network/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsn.book_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
